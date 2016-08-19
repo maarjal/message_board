5d82329f22741fa32aca4ee7034778c87f173d60
Maarja Laineste <maarja.laineste@gmail.com>
2016-08-18 21:34:14 -0700

    can add questions

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        content: this.get('content') ? this.get('content'):"",
+        author: this.get('author') ? this.get('author'):"",
+        notes: this.get('notes') ? this.get('notes'):"",
+      };
+      this.set('addQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
