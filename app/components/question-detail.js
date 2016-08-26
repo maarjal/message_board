6f27e042571944b2a0d3a27afc56c4dc17ac1bcf
Maarja Laineste <maarja.laineste@gmail.com>
2016-08-26 09:12:14 -0700

    clean up code

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -2,11 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favorites: Ember.inject.service(),
-  questionFavorited: false,
   actions: {
     addToFavorites(question) {
       this.get('favorites').add(question);
-      this.set('questionFavorited', true);
     }
   }
 });
