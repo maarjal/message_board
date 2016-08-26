6126758fded878cb41f0048a7ec59f45e39eebae
Maarja Laineste <maarja.laineste@gmail.com>
2016-08-25 23:19:28 -0700

    add favorite question service, not saving when changing route

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favorites: Ember.inject.service(),
+  questionFavorited: false,
+  actions: {
+    addToFavorites(question) {
+      this.get('favorites').add(question);
+      this.set('questionFavorited', true);
+    }
+  }
 });
